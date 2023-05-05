7927696afcd323d0e4bcd681eec62c15c7fdd548
Cordelia Payson <cordelia.payson@gmail.com>
2023-05-05 12:21:35 -0400

    linking pages in header

--- components/header.js
+++ components/header.js
@@ -1,41 +1,45 @@
-export default function Header() {
+import Link from 'next/link';
+
+function Header() {
   return (
     <div className="navbar bg-base-100">
 
       {/* regular nav bar */}
       <div className="navbar-start hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a>Home</a></li>
-          <li><a>About</a></li>
-          <li><a>NS Queer History</a></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/history">NS Queer History</Link></li>
         </ul>
       </div>
-      <a className="navbar-center hidden lg:flex btn btn-ghost normal-case text-xl">Jess Wilton</a>
+      <Link href="/" className="navbar-center hidden lg:flex btn btn-ghost normal-case text-xl">Jess Wilton</Link>
       <div className="navbar-end hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a>Research</a></li>
-          <li><a>CV</a></li>
-          <li><a>Contact</a></li>
+          <li><Link href="/research">Research</Link></li>
+          <li><Link href="/cv">CV</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
         </ul>
       </div>
 
       {/* small screen/mobile with dropdown */}
-      <a className="navbar-center lg:hidden btn btn-ghost normal-case text-xl">Jess Wilton</a>
+      <Link href="/" className="navbar-center lg:hidden btn btn-ghost normal-case text-xl">Jess Wilton</Link>
       <div className="navbar-end">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Home</a></li>
-            <li><a>About</a></li>
-            <li><a>NS Queer History</a></li>
-            <li><a>Research</a></li>
-            <li><a>CV</a></li>
-            <li><a>Contact</a></li>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/history">NS Queer History</Link></li>
+            <li><Link href="/research">Research</Link></li>
+            <li><Link href="/cv">CV</Link></li>
+            <li><Link href="/contact">Contact</Link></li>
           </ul>
         </div>
       </div>
     </div>
   );
 }
+
+export default Header;
--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ function About() {
     <Layout>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
-          <Image src="/jess1.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+          {/* <Image src="/jess1.jpg" className="max-w-sm rounded-lg shadow-2xl" /> */}
           <div>
             <h1 className="text-5xl font-bold">About</h1>
             <p className="py-6">Provident cupiditat voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>

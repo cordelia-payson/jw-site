3cbcb4f4cca50f126a1329e06cf76a08f8af223b
Cordelia Payson <cordelia.payson@gmail.com>
2023-05-05 11:10:37 -0400

    added Research page

---
+++ pages/research.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import Layout from '../components/layout.js';
+
+function Research() {
+  return (
+    <Layout>
+      <div>Research</div>
+    </Layout>
+  );
+}
+
+export default Research;

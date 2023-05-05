1c5ebf849a706369377918949a5da77488ad701d
Cordelia Payson <cordelia.payson@gmail.com>
2023-05-05 11:04:45 -0400

    created basic About page

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Layout from '../components/layout.js';
+
+function About() {
+  return (
+    <Layout>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <Image src="/jess1.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">About</h1>
+            <p className="py-6">Provident cupiditat voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default About;

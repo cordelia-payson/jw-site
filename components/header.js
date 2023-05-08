1a8066101d152eec201c12dee54b81bdc56ca26c
Cordelia Payson <cordelia.payson@gmail.com>
2023-05-08 14:57:03 -0400

    header is now evenly spaced across top of page

--- components/header.js
+++ components/header.js
@@ -4,28 +4,29 @@ function Header() {
   return (
     <div className="navbar bg-base-100">
 
-      {/* regular nav bar */}
-      <div className="navbar-start hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+      <div className="navbar-start">
+
+        <ul className="menu menu-horizontal px-1 lg:flex hidden">
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About</Link></li>
           <li><Link href="/history">NS Queer History</Link></li>
         </ul>
+
+        <Link href="/" className="navbar-center lg:hidden btn btn-ghost normal-case text-xl">Jess Wilton</Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <Link href="/" className="btn btn-ghost normal-case text-xl">Jess Wilton</Link>
+
+      <div className="navbar-center">
+        <Link href="/" className="btn btn-ghost normal-case text-xl lg:flex hidden">Jess Wilton</Link>
       </div>
-      <div className="navbar-end hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+
+      <div className="navbar-end">
+
+        <ul className="menu menu-horizontal px-1 lg:flex hidden">
           <li><Link href="/research">Research</Link></li>
           <li><Link href="/cv">CV</Link></li>
           <li><Link href="/contact">Contact</Link></li>
         </ul>
-      </div>
 
-      {/* small screen/mobile with dropdown */}
-      <Link href="/" className="navbar-center lg:hidden btn btn-ghost normal-case text-xl">Jess Wilton</Link>
-      <div className="navbar-end">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>

d3485fd188d18c1b2c2fb1de81c0b7c0540a45e2
Cordelia Payson <cordelia.payson@gmail.com>
2023-05-12 14:03:26 -0400

    current page is now underlined in header

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import HeaderLink from './link.js';
 import NameLink from './nameLink.js';
 
 function Header() {
+  const router = useRouter();
   return (
     <div className="navbar bg-base-100 border-b border-black">
 
@@ -34,7 +36,7 @@ function Header() {
           <li><HeaderLink page="/contact" name="Contact" /></li>
         </ul>
 
-        <div className="dropdown">
+        <div className="dropdown dropdown-bottom dropdown-end">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
--- components/layout/link.js
+++ components/layout/link.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function HeaderLink({ page, name }) {
+  const router = useRouter();
   return (
     <div className="bg-transparent link-hover">
-      <Link href={page}>{name}</Link>
+      <Link href={page} className={`${router.pathname === page && 'underline'}`}>{name}</Link>
     </div>
   );
 }

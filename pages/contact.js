451f22bfb9ab825c8e9ffb5b23d9d787382ae33e
Cordelia Payson <cordelia.payson@gmail.com>
2023-07-26 15:26:28 -0400

    redoing contact page

--- pages/contact.js
+++ pages/contact.js
@@ -7,11 +7,14 @@ function Contact() {
     <Layout>
       <div className="hero min-h-screen bg-base-100">
         <div className="hero-content text-center">
-          <div className="max-w-md">
+          <div className="max-w-md flex flex-col">
             <h1 className="text-5xl font-bold py-4">Get in touch!</h1>
-            <p className="">jesswilton@email.com</p>
-            <p className="">jesswilton@email.com</p>
-            <p>social media icons</p>
+            <button type="button">
+              <a className="link" href="mailto:jwilton@yorku.ca" target="_blank" rel="noreferrer noopener">jwilton@yorku.ca</a>
+            </button>
+            <button type="button">
+              <a className="link" href="https://www.linkedin.com/in/jess-wilton-938789266/">LinkedIn</a>
+            </button>
           </div>
         </div>
       </div>

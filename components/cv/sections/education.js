afb9e0a1eb9045277d4d2c5b5f136111ba8d83b6
Cordelia Payson <cordelia.payson@gmail.com>
2023-08-09 11:43:21 -0400

    moved info to separate file

--- components/cv/sections/education.js
+++ components/cv/sections/education.js
@@ -2,24 +2,38 @@ import Title from '../title.js';
 import Year from '../year.js';
 import Subtitle from '../subtitle.js';
 import Bullet from '../bullet.js';
+import { york, queens, kings } from '../info.js';
 
 function Education() {
-  const york = {
-    year: '2021-Current',
-    bold: 'PhD in History,',
-    italic: 'York University, Toronto ON',
-    bullets: ['Major Comprehensive Fields: Canadian History & Gender, Women, & Sexualities', 'Minor Field: Cultural History', 'In-progress, Preliminary Title: Archival Practice and Ephemera in the History of Nova Scotia’s Gay and Lesbian Liberation Movement, 1960-1996'],
-  };
-
   return (
     <div className="flex flex-col">
       <Title title="Education" />
-      <div className="flex flex-row">
-        <Year year={york.year} />
-        <div className="flex flex-col">
-          <Subtitle bold={york.bold} italic={york.italic} />
-          {york.bullets.map((bullet) => <Bullet bullet={bullet} />) }
+      <div className="flex flex-col">
+
+        <div className="flex flex-row">
+          <Year year={york.year} />
+          <div className="flex flex-col">
+            <Subtitle bold={york.bold} italic={york.italic} />
+            {york.bullets.map((bullet) => <Bullet bullet={bullet} />) }
+          </div>
         </div>
+
+        <div className="flex flex-row">
+          <Year year={queens.year} />
+          <div className="flex flex-col">
+            <Subtitle bold={queens.bold} italic={queens.italic} />
+            {queens.bullets.map((bullet) => <Bullet bullet={bullet} />) }
+          </div>
+        </div>
+
+        <div className="flex flex-row">
+          <Year year={kings.year} />
+          <div className="flex flex-col">
+            <Subtitle bold={kings.bold} italic={kings.italic} />
+            {kings.bullets.map((bullet) => <Bullet bullet={bullet} />) }
+          </div>
+        </div>
+
       </div>
     </div>
   );

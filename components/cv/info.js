8f8a5b492548b4c9c20713dd10d2969058d461d1
Cordelia Payson <cordelia.payson@gmail.com>
2023-08-24 15:57:41 -0400

    reformatted education to align with other sections

--- components/cv/info.js
+++ components/cv/info.js
@@ -1,24 +1,24 @@
 // education
-export const york = {
-  year: '2021-Current',
-  bold: 'PhD in History,',
-  italic: 'York University, Toronto ON',
-  bullets: ['Major Comprehensive Fields: Canadian History & Gender, Women, & Sexualities', 'Minor Field: Cultural History', 'In-progress, Preliminary Title: Archival Practice and Ephemera in the History of Nova Scotia’s Gay and Lesbian Liberation Movement, 1960-1996'],
-};
-
-export const queens = {
-  year: '2019-2021',
-  bold: 'MA in History, ',
-  italic: 'Queen’s University, Kingston ON',
-  bullets: ['Thesis title: “Alive & Kicking”: Queer Cultural Memory of Toronto’s Gay and Lesbian Liberation Movement Through Photographs of Violence and Protest in The Body Politic (1971-1987)'],
-};
-
-export const kings = {
-  year: '2014-2018',
-  bold: 'BAH in History, ',
-  italic: 'University of King’s College, Halifax NS',
-  bullets: ['Minor in Contemporary Studies', 'Certificate in Art History and Visual Culture', 'Honours Thesis Title: “Where There’s Smoke, There’s Fire: Iconic Photography and Historical Memory of the Berlin Wall”'],
-};
+export const education = [
+  {
+    year: '2021-Current',
+    bold: 'PhD in History,',
+    italic: 'York University, Toronto ON',
+    bullets: ['Major Comprehensive Fields: Canadian History & Gender, Women, & Sexualities', 'Minor Field: Cultural History', 'In-progress, Preliminary Title: Archival Practice and Ephemera in the History of Nova Scotia’s Gay and Lesbian Liberation Movement, 1960-1996'],
+  },
+  {
+    year: '2019-2021',
+    bold: 'MA in History, ',
+    italic: 'Queen’s University, Kingston ON',
+    bullets: ['Thesis title: “Alive & Kicking”: Queer Cultural Memory of Toronto’s Gay and Lesbian Liberation Movement Through Photographs of Violence and Protest in The Body Politic (1971-1987)'],
+  },
+  {
+    year: '2019-2021',
+    bold: 'MA in History, ',
+    italic: 'Queen’s University, Kingston ON',
+    bullets: ['Thesis title: “Alive & Kicking”: Queer Cultural Memory of Toronto’s Gay and Lesbian Liberation Movement Through Photographs of Violence and Protest in The Body Politic (1971-1987)'],
+  },
+];
 
 // awards
 export const awards = [['2023', 'York Graduate Award'], ['2022', 'York Graduate Award'], ['2021', 'Women’s Canadian Historical Society of Toronto Graduate Scholarship'], ['2021', 'York Graduate Award'], ['2020', 'Queen’s Graduate Award'], ['2019', 'Queen’s Graduate Award'], ['2018', 'First Class Honours (BAH)'], ['2017', 'King’s President List'], ['2015', 'Picket Trust Scholarship'], ['2014', 'Henry S Cousins Scholarship']];
--- components/cv/sections/education.js
+++ components/cv/sections/education.js
@@ -2,38 +2,22 @@ import Title from '../title.js';
 import Year from '../year.js';
 import Subtitle from '../subtitle.js';
 import Bullet from '../bullet.js';
-import { york, queens, kings } from '../info.js';
+import { education } from '../info.js';
 
 function Education() {
   return (
     <div className="flex flex-col ml-0">
       <Title title="Education" />
       <div className="flex flex-col">
-
-        <div className="flex flex-row">
-          <Year year={york.year} />
-          <div className="flex flex-col">
-            <Subtitle bold={york.bold} italic={york.italic} />
-            {york.bullets.map((bullet) => <Bullet bullet={bullet} />) }
+        {education.map((school) => (
+          <div className="flex flex-row">
+            <Year year={school.year} />
+            <div className="flex flex-col">
+              <Subtitle bold={school.bold} italic={school.italic} />
+              {school.bullets.map((bullet) => <Bullet bullet={bullet} />)}
+            </div>
           </div>
-        </div>
-
-        <div className="flex flex-row">
-          <Year year={queens.year} />
-          <div className="flex flex-col">
-            <Subtitle bold={queens.bold} italic={queens.italic} />
-            {queens.bullets.map((bullet) => <Bullet bullet={bullet} />) }
-          </div>
-        </div>
-
-        <div className="flex flex-row">
-          <Year year={kings.year} />
-          <div className="flex flex-col">
-            <Subtitle bold={kings.bold} italic={kings.italic} />
-            {kings.bullets.map((bullet) => <Bullet bullet={bullet} />) }
-          </div>
-        </div>
-
+        ))}
       </div>
     </div>
   );

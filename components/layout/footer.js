b063073089ab04cdc29243416835b5f60014683c
Cordelia Payson <cordelia.payson@gmail.com>
2023-08-30 16:43:35 -0400

    added link to my portfolio

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -2,7 +2,13 @@ export default function Footer() {
   return (
     <footer className="footer items-center p-4 bg-accent text-neutral-content">
       <div className="items-center grid-flow-col">
-        <p>Built by Cordelia Payson</p>
+        <p>
+          Built by
+          {' '}
+          <button type="button">
+            <a className="link" href="https://www.cordeliapayson.com" target="_blank" rel="noreferrer noopener">Cordelia Payson</a>
+          </button>
+        </p>
       </div>
       <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <a href="linkedin">linkedin</a>

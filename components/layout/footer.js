e0904a1e3714b15566080f081d13117b38f93c38
Cordelia Payson <cordelia.payson@gmail.com>
2023-08-30 16:51:56 -0400

    added copyright to footer. moved text to middle

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer items-center p-4 bg-accent text-neutral-content">
+    <footer className="footer footer-center p-4 bg-accent text-neutral-content">
       <div className="items-center grid-flow-col">
         <p>
           Built by
@@ -9,10 +9,7 @@ export default function Footer() {
             <a className="link" href="https://www.cordeliapayson.com" target="_blank" rel="noreferrer noopener">Cordelia Payson</a>
           </button>
         </p>
-      </div>
-      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a href="linkedin">linkedin</a>
-        <a href="social media">social media</a>
+        <p className="ml-4">Copyright © 2023 - All right reserved</p>
       </div>
     </footer>
   );
